7abeaf1acaebea2d82159098e1754185ecba86d0
Lewis Savage <lewissavage88@gmail.com>
2022-12-05 21:44:54 +0000

    Day 1 Part 1

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+var max = 0;
+var sum = 0;
+
+foreach (var line in File.ReadLines("input.txt"))
+{
+    if (line == "")
+    {
+        if (sum > max) max = sum;
+        sum = 0;
+    }
+    else
+    {
+        sum += int.Parse(line);
+    }
+}
+
+Console.WriteLine(max);
