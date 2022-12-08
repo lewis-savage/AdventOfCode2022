f304674904b9f93b3616107b2ab27e16fea54e46
Lewis Savage <lewis.savage@snapon.com>
2022-12-08 10:42:23 +0000

    Day 3 Part 1

---
+++ AdventOfCode/Day3/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int GetPriority(char character)
+{
+    if (character >= 'a')
+    {
+        return character - 'a' + 1;
+    }
+
+    return character - 'A' + 27;
+}
+
+char[][] SplitLine(string line)
+{
+    var half = line.Length / 2;
+    return new[] { line[..half].Distinct().ToArray(), line[half..].Distinct().ToArray() };
+}
+
+var sum = 0;
+
+foreach (var line in File.ReadLines("input.txt"))
+{
+    var compartments = SplitLine(line);
+    var hashSet = new HashSet<char>();
+    foreach (var compartment in compartments)
+    {
+        foreach (var c in compartment)
+        {
+            if (!hashSet.Add(c))
+            {
+                sum += GetPriority(c);
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine(sum);
