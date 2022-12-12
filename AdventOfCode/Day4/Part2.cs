310fe43faae133c15b78f31909f86da2e37cf8e2
Lewis Savage <lewis.savage@snapon.com>
2022-12-12 12:53:54 +0000

    Day 4 Part 2

---
+++ AdventOfCode/Day4/Part2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal static class Part2
+    {
+        public static void Run()
+        {
+            int overlap = 0;
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                var elves = line.Split(',');
+                var section1Split = elves[0].Split('-');
+                var section2Split = elves[1].Split('-');
+
+                var section1 = (start: int.Parse(section1Split[0]), end: int.Parse(section1Split[1]));
+                var section2 = (start: int.Parse(section2Split[0]), end: int.Parse(section2Split[1]));
+                var sec1HashSet = new HashSet<int>();
+                for (int i = section1.start; i <= section1.end; i++)
+                {
+                    sec1HashSet.Add(i);
+                }
+
+                for (int i = section2.start; i <= section2.end; i++)
+                {
+                    if (!sec1HashSet.Contains(i)) continue;
+
+                    overlap++;
+                    break;
+                }
+            }
+            Console.WriteLine(overlap);
+        }
+    }
+}
--- AdventOfCode/Day4/Program.cs
+++ AdventOfCode/Day4/Program.cs
@@ -2,3 +2,4 @@
 using Day4;
 
 Part1.Run();
+Part2.Run();
