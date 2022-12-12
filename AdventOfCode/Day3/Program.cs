8b51cee8e8da930c18fada41d6d87ead4216915d
Lewis Savage <lewis.savage@snapon.com>
2022-12-12 09:46:27 +0000

    Day 3 Part 1 cleanup

--- AdventOfCode/Day3/Program.cs
+++ AdventOfCode/Day3/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-int GetPriority(char character)
+﻿int GetPriority(char character)
 {
     if (character >= 'a')
     {
