2248a1bf4f62b9980866d334859a713f505158c2
Lewis Savage <lewis.savage@snapon.com>
2022-12-13 14:12:55 +0000

    day 6 cleanup

--- AdventOfCode/Day6/Part2.cs
+++ AdventOfCode/Day6/Part2.cs
@@ -1,20 +1,20 @@
 ﻿namespace Day6
 {
-    internal static class Part1
+    internal static class Part2
     {
         public static void Run()
         {
             using FileStream stream = File.OpenRead("input.txt");
-            const int MarkerLength = 4;
+            const int MarkerLength = 14;
             var shiftRegister = new char?[MarkerLength];
             int streamIndex = 0;
-            
-            while(true)
+
+            while (true)
             {
                 var characterByte = stream.ReadByte();
                 if (characterByte == -1) break;
                 var character = (char)characterByte;
-                if(streamIndex > MarkerLength)
+                if (streamIndex > MarkerLength)
                 {
                     if (shiftRegister.Distinct().Count() == MarkerLength)
                     {
@@ -23,7 +23,7 @@
                     }
                 }
                 Array.Copy(shiftRegister, 1, shiftRegister, 0, shiftRegister.Length - 1);
-                shiftRegister[MarkerLength-1] = character;
+                shiftRegister[MarkerLength - 1] = character;
                 streamIndex++;
             }
         }
